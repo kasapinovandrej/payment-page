0061b42f857b79825fd415b9e7f1dca4bed32c33
Andrej Kasapinov <72466548+kasapinovandrej@users.noreply.github.com>
2022-02-22 14:46:40 +0100

    new design and added flipping card

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-'useStrict'
+"use strict";
+
+const questionmark = document.querySelector(".cvv-questionmark");
+const infoMessage = document.querySelector(".cvv-questionmark-info");
+const creditCard = document.querySelector(".flip-card-inner");
+
+questionmark.addEventListener("mouseenter", () => {
+  infoMessage.classList.add("opacity");
+  creditCard.classList.add("rotate");
+});
+questionmark.addEventListener("mouseout", () => {
+  infoMessage.classList.remove("opacity");
+  creditCard.classList.remove("rotate");
+});
